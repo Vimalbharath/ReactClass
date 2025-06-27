16f0ae085f7c1e6548d92149ab8de95779208aea
Vimalbharath <vimalbharath21@gmail.com>
2025-06-27 12:20:51 +0530

    first task done

---
+++ Testing/my-react-app/src/task1/HomePage.jsx
@@ -0,0 +1,12 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Welcome to My React Home Page</h1>
+      <p>This is your first component with custom CSS styling.</p>
+    </div>
+  );
+}
+
+export default HomePage;
