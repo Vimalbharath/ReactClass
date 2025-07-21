5e26791593538803f6bda1c97a8c05c3bdce2fc9
Vimalbharath <vimalbharath21@gmail.com>
2025-07-21 12:54:06 +0530

    redux demo

--- Testing/my-react-app/src/main.jsx
+++ Testing/my-react-app/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './task7redux/App'
+import App from './task7reduxex/App'
 import { Provider } from 'react-redux';
-import store from'./task7redux/app/store';
+import store from'./task7reduxex/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
