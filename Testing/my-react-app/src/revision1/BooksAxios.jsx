22a1e08f05c72beea32ed5fee70c50d3bddbd6c4
Vimalbharath <vimalbharath21@gmail.com>
2026-06-26 20:33:56 +0530

    map issue duplicate id

--- Testing/my-react-app/src/revision1/BooksAxios.jsx
+++ Testing/my-react-app/src/revision1/BooksAxios.jsx
@@ -2,21 +2,42 @@ import { useState, useEffect } from "react";
 
 
 function BooksAxios(){
-    const [books,setBooks]=useState({});
+    const [books,setBooks]=useState([]);
     const URL='http://localhost:3006/books'
     async function fetchBooks(){
         const response=await fetch(URL);
         console.log(response)
         const data=await response.json();
         setBooks(data);
+          console.log(books);
     }
     useEffect(()=>{
         fetchBooks();
-        console.log(books);
+      
     },[])
+     // 2. Separate useEffect to log books ONLY after state has officially updated
+    useEffect(() => {
+        console.log("Books state updated successfully:", books);
+    }, [books]); 
+
+    // book=books.map((id,book)=>{
+    //         return (
+    //             <div>
+                    
+    //             </div>
+    //         )
+    //     })
+
+    const vimal=["apple","mango","orange"]
+
     return (
-        <div>
         
+        <div>
+           {vimal.map((v,id)=>{
+            return(
+                <div key='id'>{v}</div>
+            )
+           })}
         </div>
     )
 }
