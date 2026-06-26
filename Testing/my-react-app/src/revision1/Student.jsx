c8e10e3c02b374244e43204f7092f6721495457c
Vimalbharath <vimalbharath21@gmail.com>
2026-06-26 08:44:15 +0530

    class props

--- Testing/my-react-app/src/revision1/Student.jsx
+++ Testing/my-react-app/src/revision1/Student.jsx
@@ -1,9 +1,16 @@
-function Student(props){
-    return (
-        <div>
-            The Student name is : {props.name}
+import React from "react";
+
+class Student extends React.Component{
+    render() {
+        return (
+             <div>
+            The Student name is : {this.props.name}
         </div>
-    )
+        )
+       
+    }
+        
+    
 }
 
 export default Student;
